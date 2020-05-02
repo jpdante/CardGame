d073c557d742b2f91dd721e031137fbc8f6e9647
felipecmarques <64704610+felipecmarques@users.noreply.github.com>
2020-05-02 16:15:31 -0300

    Update Carta.java

--- src/br/anhembi/cardgame/model/Carta.java
+++ src/br/anhembi/cardgame/model/Carta.java
@@ -1,4 +1,47 @@
 package br.anhembi.cardgame.model;
 
 public class Carta {
+    private TipoCarta tipo;
+    private CorCarta cor;
+    private int numero;
+
+    public Carta(TipoCarta tipo){
+        this.tipo = tipo;
+    }
+
+    public Carta(TipoCarta tipo, CorCarta cor){
+        this.tipo = tipo;
+        this.cor = cor;
+    }
+
+    public Carta(TipoCarta tipo,  CorCarta cor, int numero){
+        this.tipo = tipo;
+        this.cor = cor;
+        this.numero = numero;
+    }
+
+    public TipoCarta getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCarta tipo) {
+        this.tipo = tipo;
+    }
+
+    public CorCarta getCor() {
+        return cor;
+    }
+
+    public void setCor(CorCarta cor) {
+        this.cor = cor;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
+
