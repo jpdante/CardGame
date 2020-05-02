09f65f1fdd0e4ef1e6ae1d7b3b03f67e43358f81
felipecmarques <64704610+felipecmarques@users.noreply.github.com>
2020-05-02 17:11:35 -0300

    Update Main.java

--- src/br/anhembi/cardgame/Main.java
+++ src/br/anhembi/cardgame/Main.java
@@ -1,11 +1,45 @@
 package br.anhembi.cardgame;
+import br.anhembi.cardgame.manager.UnoManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         new Main().run(args);
     }
 
-    public void run(String[] args) {
+    private boolean running;
+    private Scanner in;
+    private UnoManager unoManager;
 
+
+
+    public void run(String[] args) {
+        running = true;
+        in = new Scanner(System.in);
+        unoManager = new UnoManager();
+
+        unoManager.inicializarBaralho();
+        //unoManager.EmbaralharBaralho();
+    
+        while(running) {
+            mostrarComandos();
+            processarComandos(in.nextLine());
+        }
+
+
+    }
+    public void mostrarComandos() {
+        System.out.println();
+        System.out.println("~-~-~- Comandos UNO -~-~-~");
+        System.out.println("1. Embaralhar Baralho");
+        System.out.println("2. Mostrar Baralho");
+        System.out.println("3. Adicionar jogador");
+        System.out.println("4. Mostrar Jogadores");
+        System.out.println("5. Iniciar Jogo");
+        System.out.println("6. Mostrar Mesa");
+        System.out.println("0. Sair");
+        System.out.println();
+        System.out.print(">");
     }
 }
